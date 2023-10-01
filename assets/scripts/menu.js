593719f9017a024d8a3804242fd18e067fad1cb9
eumerme <euthaisetau@gmail.com>
2023-09-30 23:28:54 -0300

    fix: toggle mobile menu

--- assets/scripts/menu.js
+++ assets/scripts/menu.js
@@ -10,23 +10,22 @@ function toggleActive(element, _class) {
 	element.classList.toggle(_class);
 }
 
-// Alterna o estado do menu (aberto/fechado) ao clicar no ícone de menu
-menuIcon.addEventListener("click", () => {
+function toglleMenu() {
 	toggleActive(menuIcon, CLOSE_ICON);
 	toggleActive(navbar, ACTIVE);
 	toggleActive(header, ACTIVE);
 	toggleActive(modal, ACTIVE);
-});
+}
 
-// Fecha o menu se o usuário clicar fora da navbar
+// Alterna o estado do menu (aberto/fechado) ao clicar no ícone de menu
+menuIcon.addEventListener("click", () => toglleMenu());
+
+// Fecha o menu se o usuário clicar fora do model do menu ou em algum link
 document.addEventListener("click", function (event) {
-	const isInsideNavbar = navbar.contains(event.target);
-	const isInsideMenuIcon = menuIcon.contains(event.target);
-
-	if (!isInsideNavbar && !isInsideMenuIcon) {
-		toggleActive(menuIcon, CLOSE_ICON);
-		toggleActive(navbar, ACTIVE);
-		toggleActive(header, ACTIVE);
-		toggleActive(modal, ACTIVE);
+	const isMenuIcon = event.target === menuIcon;
+	if (!isMenuIcon) {
+		const isInsideModal = modal.contains(event.target);
+		const isInsideHeader = header.contains(event.target);
+		if (isInsideModal || isInsideHeader) toglleMenu();
 	}
 });
